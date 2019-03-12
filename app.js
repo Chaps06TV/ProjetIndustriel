405d304706c475bd355fa2a9760d888f570b5f8e
Chaps06TV <jasminchapados@gmail.com>
2019-03-11 22:16:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var con = mysql.createConnection({
   database: "bdd_industriel"
 });
 
+var connectionList = new Array(2);
+
 // Chargement de la page index.html
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
@@ -33,14 +35,17 @@ app.get('/cuves', function (req, res) {
   res.sendfile(__dirname + '/cuves.html');
 });
 
-
 io.sockets.on('connection', function(socket){
-	// Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+	SaveConnection(socket.id);
+	console.log('\x1b[33m%s\x1b[37m%s\x1b[0m', GetConnection(socket.id), " connected");
+	console.log('Connections:');
+	console.log(connectionList);
+
 	socket.on('load_Cb', function() {
 		
 		con.connect(function(err) {
 		  var sql = "SELECT clientId, prenom, nom FROM tblclient ORDER BY nom";
-  		  console.log("load_Cb: " + sql);
+  		  console.log('\x1b[33m%s\x1b[37m%s\x1b[0m', GetConnection(socket.id), " { load_Cb: " + sql + " } ");
 		  con.query(sql, function (err, result) {
 		  socket.emit('reponse_load_Cb',{result});
 			});
@@ -50,7 +55,7 @@ io.sockets.on('connection', function(socket){
 	socket.on('load_client', function(data) {
 		con.connect(function(err) {
 		  var sql = "SELECT prenom, nom, adresse FROM tblclient WHERE clientID=" + data.idClient;
-		  console.log("load_client: " + sql);
+		  console.log('\x1b[33m%s\x1b[37m%s\x1b[0m', GetConnection(socket.id), " { load_client: " + sql + " } ");
 		  con.query(sql, function (err, result) {
 		  socket.emit('reponse_load_client',{result});
 			});
@@ -60,7 +65,7 @@ io.sockets.on('connection', function(socket){
 	socket.on('load_commande', function(data) {
 		con.connect(function(err) {
 		  var sql = "SELECT nom, quantite, echeance FROM tblProduction WHERE productId=" + data.idCommande;
-		  console.log("load_commande: " + sql);
+		  console.log('\x1b[33m%s\x1b[37m%s\x1b[0m', GetConnection(socket.id), " { load_commande: " + sql + " } ");
 		  con.query(sql, function (err, result) {
 		  socket.emit('reponse_load_commande',{result});
 			});
@@ -70,21 +75,20 @@ io.sockets.on('connection', function(socket){
 	socket.on('load_commande_client', function(data) {
 		con.connect(function(err) {
 		  var sql = "SELECT productID, nom FROM tblproduction WHERE clientID=" + data.idClient + " ORDER BY echeance";
-		  console.log("load_commande_client: " + sql);
+		  console.log('\x1b[33m%s\x1b[37m%s\x1b[0m', GetConnection(socket.id), " { load_commande_client: " + sql + " } ");
 		  con.query(sql, function (err, result) {
 		  socket.emit('reponse_load_commande_client',{result});
 			});
 		});
 	});
 
-	// Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
 	socket.on('nouveau_client', function(data) {
 		var prenom = data.prenom;
 		var nom = data.nom;
 		var adresse = data.adresse;
 		  con.connect(function(err) {
 		  var sql = "INSERT INTO tblclient (prenom, nom, adresse) VALUES ('" + prenom + "','" + nom + "','" + adresse + "')";
-		  console.log("nouveau_client: " + sql);
+		  console.log('\x1b[33m%s\x1b[37m%s\x1b[0m', GetConnection(socket.id), " { nouveau_client: " + sql + " } ");
 		  con.query(sql, function (err, result) {
 		  });
 		});
@@ -98,12 +102,45 @@ io.sockets.on('connection', function(socket){
 		var date = data.date;
 		  con.connect(function(err) {
 		  var sql = "INSERT INTO tblproduction (clientId, nom, quantite, echeance) VALUES ('" + clientId + "','" + nomProd + "','" + qte + "','" + date + "')";
-		  console.log("nouvelle_commande: " + sql);
+		  console.log('\x1b[33m%s\x1b[37m%s\x1b[0m', GetConnection(socket.id), " { nouvelle_commande: " + sql + " } ");
 		  con.query(sql, function (err, result) {
 		  });
 		});
 	});
 
+	socket.on('disconnect', function() {
+		console.log('\x1b[33m%s\x1b[37m%s\x1b[0m', GetConnection(socket.id), " disconnected");
+    	DelConnection(socket.id);
+	});
+
 });
 
+
 server.listen(8080);
+
+function SaveConnection(id) {
+	var idClient = 1;
+	var fini = false;
+	while(!fini) {
+		var ok = true;
+		for(var i = 2; i < connectionList.length; i++)
+			if(connectionList[i][1] == ("Client " + idClient)) { 
+				idClient++;
+				ok = false;
+			}
+		if (ok) fini = true;
+	}
+
+
+	connectionList.push([id, "Client " + idClient]);
+}
+
+function GetConnection(id) {
+	for(var i = 2; i < connectionList.length; i++)
+		if(connectionList[i][0] == id) { return connectionList[i][1];}
+}
+
+function DelConnection(id) {
+	for(var i = 2; i < connectionList.length; i++)
+		if(connectionList[i][0] == id) { connectionList.splice(i,1);}
+}
